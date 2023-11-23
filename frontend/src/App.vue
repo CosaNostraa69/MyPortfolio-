58a017dea500722d69e3ee4f34ab20ab2ea419cb
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2023-11-23 16:14:44 +0100

    Mac interface static

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,106 +1,191 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import Dock from './components/Dock.vue';
+import Menubar from 'primevue/menubar';
+import Toast from 'primevue/toast';
+import { ref } from 'vue';
+
+
+const menubarItems = ref([
+    {
+        label: 'Finder',
+        class: 'menubar-root'
+    },
+    {
+        label: 'File',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+                items: [
+                    {
+                        label: 'Bookmark',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label: 'Video',
+                        icon: 'pi pi-fw pi-video'
+                    },
+
+                ]
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Export',
+                icon: 'pi pi-fw pi-external-link'
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        items: [
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            },
+
+        ]
+    },
+    {
+        label: 'Users',
+        items: [
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-user-plus',
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-user-minus',
+
+            },
+            {
+                label: 'Search',
+                icon: 'pi pi-fw pi-users',
+                items: [
+                    {
+                        label: 'Filter',
+                        icon: 'pi pi-fw pi-filter',
+                        items: [
+                            {
+                                label: 'Print',
+                                icon: 'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'pi pi-fw pi-bars',
+                        label: 'List'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Events',
+        items: [
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {
+                        label: 'Save',
+                        icon: 'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            },
+            {
+                label: 'Archive',
+                icon: 'pi pi-fw pi-calendar-times',
+                items: [
+                    {
+                        label: 'Remove',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Quit'
+    }
+]);
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <Menubar
+        :model="menubarItems"
+        :start="start"
+        :end="end"        
+
+
+      />
   </header>
+  <Toast 
+    ref="toast"
+    position="bottom-right"
+    :group="true"
+    :life="3000"
+    
+  />
+  <div class="dock-window dock-advanced">
+                <Dock :model="items">
+                    <template #item="{ item }">
+                        <a v-tooltip.top="item.label" href="#" class="p-dock-link" @click="onDockItemClick($event, item)">
+                            <img :alt="item.label" :src="item.icon" style="width: 100%" />
+                        </a>
+                    </template>
+                </Dock>
+  </div>
 </template>
 
 <style scoped>
 header {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box; /* Assure que padding et border sont inclus dans la largeur/hauteur */
+    height: 4rem;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 100;
 }
 
-.wrapper {
-  margin-top: auto;
-  width: 80%; /* Empêche le débordement horizontal */
-  padding: 10px; /* Exemple de padding, ajustez selon vos besoins */
-  box-sizing: border-box;
-  background-color: white;
-  border-radius: 5px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  opacity: 30%;
-}
-
-nav {
-  display: flex;
-  flex-direction: row; /* Disposer les éléments horizontalement */
-  justify-content: center; /* Centrer les liens horizontalement */
-  gap: 20px; 
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a {
-  text-decoration: none;
-  color: #000; 
-}
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.dock-window {
+    background-color: #241919;
+    bottom: 0;
+    left: 0;
+    position: fixed;
+    width: 100%;
+    padding: 0;
+    
 }
 
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.menubar-root {
+    font-size: 1.5rem;
+    font-weight: bold;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,20 @@
 import './assets/main.css'
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 import router from './router'
-
-
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css'; 
+import 'primevue/resources/primevue.min.css'; 
+import Dock from 'primevue/dock';
 
 const app = createApp(App)
 
 app.use(router);
 app.use(PrimeVue);
 app.use(ToastService);
+app.component('Dock', Dock);
+
 
 
 app.mount('#app')
